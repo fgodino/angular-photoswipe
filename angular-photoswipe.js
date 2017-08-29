b0c036092fbfe53f3f41e4f1148dfd7eddfdda88
Fernando Godino <fernando.godino@gmail.com>
2017-08-29 16:03:53 -0500

    Minify safe

--- angular-photoswipe.js
+++ angular-photoswipe.js
@@ -14,104 +14,102 @@
 
     return angular
       .module('ngPhotoswipe', [])
-      .directive('ngPhotoswipe', ngPhotoswipeDirective);
-
-    function ngPhotoswipeDirective($compile, $http, $templateRequest) {
-      return {
-        restrict: 'AE',
-        replace: true,
-        scope: {
-          open: '=',
-          options: '=',
-          slides: '=',
-          slideSelector: '@',
-          template: '@',
-          onClose: '&',
-          onInit: '&'
-        },
-        link: linkFn
-      };
-
-      function linkFn(scope, iElement, iAttrs) {
-        scope.template = scope.template || 'views/ng-photoswipe.html';
-
-        $templateRequest(scope.template)
-          .then(function(html){
-            var template = angular.element(html);
-            iElement.append($compile(template)(scope));
-          });
-
-        scope.start = function () {
-          scope.open = true;
-          startGallery();
+      .directive('ngPhotoswipe', ['$compile', '$http', '$templateRequest', function ($compile, $http, $templateRequest) {
+        return {
+          restrict: 'AE',
+          replace: true,
+          scope: {
+            open: '=',
+            options: '=',
+            slides: '=',
+            slideSelector: '@',
+            template: '@',
+            onClose: '&',
+            onInit: '&'
+          },
+          link: linkFn
         };
 
-        var startGallery = function () {
-          var pswpElement = document.querySelectorAll('.pswp')[0];
+        function linkFn(scope, iElement, iAttrs) {
+          scope.template = scope.template || 'views/ng-photoswipe.html';
 
-          if (angular.isUndefined(scope.options.getThumbBoundsFn) &&
-              angular.isDefined(scope.slideSelector)) {
+          $templateRequest(scope.template)
+            .then(function(html){
+              var template = angular.element(html);
+              iElement.append($compile(template)(scope));
+            });
 
-            scope.options = angular.merge({}, {
+          scope.start = function () {
+            scope.open = true;
+            startGallery();
+          };
 
-              getThumbBoundsFn: function(index) {
-                var thumbnail = document.querySelectorAll(scope.slideSelector)[index];
-                var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
-                var rect = thumbnail.getBoundingClientRect();
-                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+          var startGallery = function () {
+            var pswpElement = document.querySelectorAll('.pswp')[0];
+
+            if (angular.isUndefined(scope.options.getThumbBoundsFn) &&
+                angular.isDefined(scope.slideSelector)) {
+
+              scope.options = angular.merge({}, {
+
+                getThumbBoundsFn: function(index) {
+                  var thumbnail = document.querySelectorAll(scope.slideSelector)[index];
+                  var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+                  var rect = thumbnail.getBoundingClientRect();
+                  return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+                }
+
+              }, scope.options);
+            }
+
+            scope.gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default || false, scope.slides, scope.options);
+            scope.gallery.init();
+            scope.item = scope.gallery.currItem;
+
+            scope.gallery.listen('destroy', function () {
+              scope.safeApply(function () {
+                (scope.onClose || angular.noop)();
+              });
+            });
+
+            scope.gallery.listen('afterChange', function () {
+              scope.safeApply(function () {
+                scope.item = scope.gallery.currItem;
+              });
+            });
+
+            scope.safeApply(function () {
+              (scope.onInit || angular.noop)({ gallery: scope.gallery });
+            });
+          };
+
+          scope.$watch('open', function (nVal, oVal) {
+            if (nVal != oVal) {
+              if (nVal) {
+                startGallery();
               }
-
-            }, scope.options);
-          }
-
-          scope.gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default || false, scope.slides, scope.options);
-          scope.gallery.init();
-          scope.item = scope.gallery.currItem;
-
-          scope.gallery.listen('destroy', function () {
-            scope.safeApply(function () {
-              (scope.onClose || angular.noop)();
-            });
-          });
-
-          scope.gallery.listen('afterChange', function () {
-            scope.safeApply(function () {
-              scope.item = scope.gallery.currItem;
-            });
-          });
-
-          scope.safeApply(function () {
-            (scope.onInit || angular.noop)({ gallery: scope.gallery });
-          });
-        };
-
-        scope.$watch('open', function (nVal, oVal) {
-          if (nVal != oVal) {
-            if (nVal) {
-              startGallery();
+            } else if (!nVal && scope.gallery) {
+              scope.gallery.close();
+              scope.gallery = null;
             }
-          } else if (!nVal && scope.gallery) {
-            scope.gallery.close();
+          });
+
+          scope.safeApply = function(fn) {
+            var phase = this.$root.$$phase;
+            if(phase == '$apply' || phase == '$digest') {
+              if(fn && (typeof(fn) === 'function')) {
+                fn();
+              }
+            } else {
+              this.$apply(fn);
+            }
+          };
+
+          scope.$on('destroy', function () {
             scope.gallery = null;
-          }
-        });
-
-        scope.safeApply = function(fn) {
-          var phase = this.$root.$$phase;
-          if(phase == '$apply' || phase == '$digest') {
-            if(fn && (typeof(fn) === 'function')) {
-              fn();
-            }
-          } else {
-            this.$apply(fn);
-          }
-        };
-
-        scope.$on('destroy', function () {
-          scope.gallery = null;
-        });
-      }
-    }
+          });
+        }
+      }]);
   }
 
   if (typeof define === 'function' && define.amd) {
